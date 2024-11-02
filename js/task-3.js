10dac5216f7c7a5605db1da6a0963eea6e6afb55
MykytaKononov <nikitamaxkononov@gmail.com>
2024-11-02 18:03:20 +0100

    done

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function filterArray(numbers, value) {
+  let final = [];
+  for (let can of numbers) {
+    if (can > value) {
+      final.push(can);
+    }
+  }
+  return final;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
